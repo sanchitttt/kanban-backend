7b2ce2ffae97cfc94cd7e8ba792e55ea2779b2ab
Sanchit Tewari <46245135+sanchitttt@users.noreply.github.com>
2023-02-13 15:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 //allow requests from anywhere
 app.use(
     cors({
-        origin: `https://kanban-sanchit.vercel.app`,
+        origin: "*",
         allowedHeaders: "Set-Cookie,Origin, X-Requested-With, Content-Type, Accept,'Authorization', 'x-csrf-token'",
         credentials: true,
     })
